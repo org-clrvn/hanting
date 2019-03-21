8c91b2a48f2feefd0f448ebba821fb0dd3a0b996
Clrvn <1605509877@qq.com>
2019-03-21 15:05:43 +0800

    第一次提交

--- src/main/java/com/sy/hting/cfg/WebConfig.java
+++ src/main/java/com/sy/hting/cfg/WebConfig.java
@@ -19,47 +19,46 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 @ServletComponentScan // 支持自定义web过滤器和servlet
 public class WebConfig implements WebMvcConfigurer {
 
-	/**
-	 * 
-	 * @title: httpMessageConverters
-	 * @description: FastJson配置
-	 * @return
-	 */
-	@Bean
-	public HttpMessageConverters httpMessageConverters() {
-		FastJsonHttpMessageConverter fjhmc = new FastJsonHttpMessageConverter();
-		fjhmc.setCharset(FastJsonHttpMessageConverter.UTF8);
-		List<MediaType> types = new ArrayList<MediaType>();
-		types.add(MediaType.APPLICATION_JSON_UTF8);
-		fjhmc.setSupportedMediaTypes(types);
-		fjhmc.setFeatures(SerializerFeature.WriteEnumUsingToString, SerializerFeature.WriteMapNullValue,
-				SerializerFeature.QuoteFieldNames, SerializerFeature.PrettyFormat,
-				SerializerFeature.WriteDateUseDateFormat, SerializerFeature.WriteNullNumberAsZero,
-				SerializerFeature.WriteNullStringAsEmpty);
-		return new HttpMessageConverters(fjhmc);
-	}
+    /**
+     * @return
+     * @title: httpMessageConverters
+     * @description: FastJson配置
+     */
+    @Bean
+    public HttpMessageConverters httpMessageConverters() {
+        FastJsonHttpMessageConverter fjhmc = new FastJsonHttpMessageConverter();
+        fjhmc.setCharset(FastJsonHttpMessageConverter.UTF8);
+        List<MediaType> types = new ArrayList<MediaType>();
+        types.add(MediaType.APPLICATION_JSON_UTF8);
+        fjhmc.setSupportedMediaTypes(types);
+        fjhmc.setFeatures(SerializerFeature.WriteEnumUsingToString, SerializerFeature.WriteMapNullValue,
+                SerializerFeature.QuoteFieldNames, SerializerFeature.PrettyFormat,
+                SerializerFeature.WriteDateUseDateFormat, SerializerFeature.WriteNullNumberAsZero,
+                SerializerFeature.WriteNullStringAsEmpty);
+        return new HttpMessageConverters(fjhmc);
+    }
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		// 设置允许跨域的路径
-		registry.addMapping("/**")
-				// 设置允许跨域请求的域名
-				.allowedOrigins("*").allowedHeaders("*")
-				// 是否允许证书 不再默认开启
-				.allowCredentials(true)
-				// 设置允许的方法
-				.allowedMethods("*");
-	}
-	
-	
-	/**
-	 * 
-	* @title: serverEndpointExporter 
-	* @description: 注册WebSocket endpoint站点服务,单元测试的时候需要注释这个配置【非常重要】
-	* @return
-	 */
-	@Bean
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 设置允许跨域的路径
+        registry.addMapping("/**")
+                // 设置允许跨域请求的域名
+                .allowedOrigins("*").allowedHeaders("*")
+                // 是否允许证书 不再默认开启
+                .allowCredentials(true)
+                // 设置允许的方法
+                .allowedMethods("*");
+    }
+
+
+    /**
+     *
+     * @title: serverEndpointExporter
+     * @description: 注册WebSocket endpoint站点服务,单元测试的时候需要注释这个配置【非常重要】
+     * @return
+     */
+	/*@Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
-    }
+    }*/
 }
